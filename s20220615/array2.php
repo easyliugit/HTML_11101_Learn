734bd8b4ce3524e780152bde30ad970ef964c449
alphaliu <easy19050711@gmail.com>
2022-06-15 13:51:10 +0800

    使用Object提取陣列資料

--- s20220615/array2.php
+++ s20220615/array2.php
@@ -10,3 +10,26 @@
         "c" => "cat",
         "d" => "dog"
     ];
+
+    
+
+    // foreach
+    foreach ($myArr as $key => $value) {
+        print_r($key);
+        print_r($value);
+
+        // php 多維陣列
+
+        $students = [
+            [
+                'id' => 1,
+                'name' => 'amy',
+                'tel' => '0911-111-111'
+            ],
+            [
+                'id' => 2,
+                'name' => 'bob',
+                'tel' => '0922-222-222'
+            ],
+        ];
+    }
