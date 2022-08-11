491628353a9acf520d57fcaa8a266e0826136523
alphaliu <easy19050711@gmail.com>
2022-08-11 16:35:22 +0800

    js外掛套件，ajax網頁爬蟲。

---
+++ s20220811/myapi2.php
@@ -0,0 +1,28 @@
+<?php
+//允許來源傳回 Access-Control-Allow-Origin 標頭
+// header("Access-Control-Allow-Origin: *");
+
+// [PHP]curl - 佛祖球球
+// https://blog.johnsonlu.org/php-curl/
+// [程式][PHP] 如何使用PHP CURL，基礎教學。
+// https://expect7.pixnet.net/blog/post/36428081
+// 初始化 curl
+$ch = curl_init("https://data.ntpc.gov.tw/api/datasets/71CD1490-A2DF-4198-BEF1-318479775E8A/json?page=0&size=10");
+//關閉SSL認證 https 要取得
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    
+// 因傳回結果為物件，前端前端JS無法直接使用，不直接顯示在畫面上，傳給前端JS記得加這行
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+//解析後轉換為字串
+$output = curl_exec($ch);
+
+//加入User Agent 模擬browser行為
+// curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:28.0) Gecko/20100101 Firefox/28.0");
+
+// close
+curl_close($ch);
+
+echo $output;
+// var_dump($output);
+?>
