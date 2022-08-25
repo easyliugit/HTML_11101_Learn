4fce7b7d78c332f46add5b8e65cdaa9285454e81
alphaliu <easy19050711@gmail.com>
2022-08-25 16:16:31 +0800

    MVC架構 建立其中的C(Controller)

--- homework/laravel/s20220824/routes/web.php
+++ homework/laravel/s20220824/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/test', [TestController::class, 'testFun']);
 
 //admin prefix
 Route::prefix('admin')->group(function () {
