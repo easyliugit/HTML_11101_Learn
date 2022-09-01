e4952d6cb07b253cbf4ba07d90776c0f1c877885
alphaliu <easy19050711@gmail.com>
2022-09-01 09:09:12 +0800

    連結css方法 <style標籤、絕對路徑、相對路徑

--- homework/laravel/s20220824/resources/views/StudentController/index.blade.php
+++ homework/laravel/s20220824/resources/views/StudentController/index.blade.php
@@ -4,9 +4,45 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    {{-- 決對路徑 --}}
+    {{-- <link rel="stylesheet" href="/laravel/public/css/style.css"> --}}
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    {{-- 相對路徑 --}}
     <title>student_index</title>
 </head>
 <body>
-    student_index page
+        
+<div class="container mt-3">
+    <h2>student_index page</h2>
+    <p>The .table class adds basic styling (light padding and horizontal dividers) to a table:</p>            
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Firstname</th>
+          <th>Lastname</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>John</td>
+          <td>Doe</td>
+          <td>john@example.com</td>
+        </tr>
+        <tr>
+          <td>Mary</td>
+          <td>Moe</td>
+          <td>mary@example.com</td>
+        </tr>
+        <tr>
+          <td>July</td>
+          <td>Dooley</td>
+          <td>july@example.com</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </body>
 </html>
